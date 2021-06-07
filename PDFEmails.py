6b9264e1783e382007191aca0b6a6fe54ad5883b
zacharyeller13 <zacharyeller@pm.me>
2021-06-07 22:02:14 +0000

    Add code to grab all .pdf files for processing

--- PDFEmails.py
+++ PDFEmails.py
@@ -3,6 +3,7 @@
 
 import re
 import os
+import glob
 
 import pdfplumber
 import pandas as pd
@@ -15,31 +16,33 @@ Line = namedtuple('Line', 'name title function direct_email direct_phone')
 contact_re = re.compile(r'[Ee]xecutives [Aa]t [Tt]his [Ll]ocation')
 line_re = re.compile(r'([a-zA-Z]+\.?\s?[a-zA-Z ]+)\s{2}([a-zA-Z ]+)\s{2}([a-zA-Z]+,?\s?[a-zA-Z]+)\s{2}([a-zA-Z\.@ ]+)\s{2}([\d-]+)')
 
-# Get file
-file = "Enbridge-Reserve Pump Station Reserve, MT.pdf"
+# Get all files in folder
+files = glob.glob('*.pdf')
 
 # initiate lines array
 lines = []
 
-# read PDF and grab line(s)
-with pdfplumber.open(file) as pdf:
-    pages = pdf.pages
-    for page in pages:
-        text = page.extract_text().split('\n')
+# for each PDF in files read PDF and grab line(s)
+for file in files:
+    
+    with pdfplumber.open(file) as pdf:
+        pages = pdf.pages
+        for page in pages:
+            text = page.extract_text().split('\n')
 
-# Read each line and search for against start of contact block
+    # Read each line and search for against start of contact block
 
-        for i, line in enumerate(text):
-            start = contact_re.search(line)
-            
-            if start:
-                contact = text[i+2]
-            
-# if contact block is found, search for contact and append to lines
-                if line_re.search(contact):
-                    items = contact.split("  ")
-                    items[3] = items[3].replace(' ', '')
-                    lines.append(Line(*items))
+            for i, line in enumerate(text):
+                start = contact_re.search(line)
+                
+                if start:
+                    contact = text[i+2]
+                
+    # if contact block is found, search for contact and append to lines
+                    if line_re.search(contact):
+                        items = contact.split("  ")
+                        items[3] = items[3].replace(' ', '')
+                        lines.append(Line(*items))
 
 # transform lines to pandas dataframe, write to a CSV
 df = pd.DataFrame(lines)
